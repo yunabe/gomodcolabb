1586c59f9edd448eb89f7a8a56b7af023d437a1c
Yu Watanabe <yunabe.public@gmail.com>
2020-04-29 14:06:24 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yunabe/gomodcolabb
+
+go 1.14
+
+require github.com/yunabe/gomodcolaba v0.0.0-20200429130743-15fa9d60f4d6
---
+++ gomodcolabb.go
@@ -0,0 +1,10 @@
+package gomodcolabb
+
+import (
+	a "github.com/yunabe/gomodcolaba"
+)
+
+// VersionOfModuleA returns the version of gomodcolaba.
+func VersionOfModuleA() string {
+	return a.Version()
+}
